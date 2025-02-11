abef52c32bb729e9a190d1ef19c44e1bec691956
chathura <chathuhiru45@gmail.com>
2025-02-11 15:56:52 +0530

    customer save done

--- Z13_Spring_Boot/src/main/java/lk/ijse/z13_spring_boot/controller/CustomerController.java
+++ Z13_Spring_Boot/src/main/java/lk/ijse/z13_spring_boot/controller/CustomerController.java
@@ -1,16 +1,19 @@
 package lk.ijse.z13_spring_boot.controller;
 
 import lk.ijse.z13_spring_boot.dto.CustomerDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.z13_spring_boot.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
-    @GetMapping
-    public String getCustomer(@RequestBody CustomerDTO customerDTO) {
-        return customerDTO.toString();
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping(path = "save")
+    public boolean getCustomer(@RequestBody CustomerDTO customerDTO) {
+        boolean res = customerService.save(customerDTO);
+        return res;
     }
 }
---
+++ Z13_Spring_Boot/src/main/java/lk/ijse/z13_spring_boot/service/CustomerService.java
@@ -0,0 +1,19 @@
+package lk.ijse.z13_spring_boot.service;
+
+import lk.ijse.z13_spring_boot.dto.CustomerDTO;
+import lk.ijse.z13_spring_boot.entity.Customer;
+import lk.ijse.z13_spring_boot.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    public boolean save(CustomerDTO customerDTO) {
+        Customer customer = new Customer(customerDTO.getId(), customerDTO.getName(), customerDTO.getAddress());
+        customerRepo.save(customer);
+        return true;
+    }
+}
